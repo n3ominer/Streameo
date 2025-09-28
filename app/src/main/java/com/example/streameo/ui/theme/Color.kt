140303ba0a98e0b0eed2c9a2d433d224434df073
Ramzy KERMAD <ramzy.kermad@edenred.com>
2025-09-28 13:26:49 +0200

    Updated Theme

--- app/src/main/java/com/example/streameo/ui/theme/Color.kt
+++ app/src/main/java/com/example/streameo/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Black900 = Color(0xFF0B0B0B)
+val Gray900 = Color(0xFF141414)
+val Gray700 = Color(0xFF2A2A2A)
+val Accent = Color(0xFFFF6B00)
+val White = Color(0xFFFFFFFF)
--- app/src/main/java/com/example/streameo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/streameo/ui/theme/Theme.kt
@@ -11,27 +11,13 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val DarkColors = darkColorScheme(
+    primary = Accent,
+    surface = Gray900,
+    background = Black900,
+    onBackground = White
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun StreameoTheme(
@@ -40,19 +26,10 @@ fun StreameoTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColors,
         typography = Typography,
+        shapes = MaterialTheme.shapes,
         content = content
     )
 }
